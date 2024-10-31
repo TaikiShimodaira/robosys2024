b7ea10bf4af96409dc4951e2c079bacbf701d258
Taiki Shimodaira <s23C1069@s.chibakoudai.jp>
2024-11-01 01:06:05 +0900

    change name

--- test.bash
+++ test.bash
@@ -10,16 +10,5 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]	|| ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
-out=$(echo | ./plus)
-[ "$?" = 1 ]	|| ng "$LINENO"
-[ "${out}" = "" ] ||ng "$LINENO"
-
-[ "$res" = 0 ] && echo OK
-
+[ "${res}" = 0 ] && echo OK
 exit $res
-
-
